cadb4f4d44704f63b995ff68798ccf606711c160
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-11-19 10:44:40 +0200

    Create tampermonkey-github-pr-file-path-copy.js

---
+++ tampermonkey-github-pr-file-path-copy.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         GitHub PR comments file path copy
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-01
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*/pull/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(() => {
+        const cpy = document.createElement("clipboard-copy");
+        for (const el of document.querySelectorAll(".review-thread-component summary div")) {
+            if (!el.querySelector("clipboard-copy")) {
+                el.innerHTML += `<clipboard-copy onclick="(arguments[0] || window.event).stopPropagation();" data-copy-feedback="Copied!" aria-label="Copy" value="${el.querySelector("a").textContent}" data-view-component="true" class="Link--onHover color-fg-muted ml-2 mr-2" tabindex="0" role="button">
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy">
+            <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+                </svg>
+        <svg style="display: none;" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success">
+            <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+        </svg>
+        </clipboard-copy>`;
+            }
+        }
+    }, 1000);
+})();
