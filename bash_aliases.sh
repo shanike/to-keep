90f4c26fd2d55eb05facd28282b2237dc36339ff
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-11-19 10:42:31 +0200

    Update bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -5,11 +5,9 @@ alias cdd="cd ../../"
 
 # GIT
 alias gs="git status"
-alias gl="git log --oneline --graph"
+alias gl="git lc"
 # git log local commits (https://stackoverflow.com/a/3338774/12056841) 
-alias gll="git log --branches --not --remotes"
-# git remove local branches that have no remote
-alias git-delete-removed-branches="git fetch -p && git branch -vv | awk \"/: gone]/{print $1}\" | xargs git branch -d"
+alias gll="git ll"
 
 # DEV PNPM
 alias pn="pnpm"
